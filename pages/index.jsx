b2f184c9a57258ff472b5f788a328a0e24af95eb
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-21 14:50:34 +0100

    writing and technologies

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,28 @@ import MobileNav from '@/components/mobileNav';
 import Link from 'next/link';
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion'
-import { SiTwitter, SiGithub } from 'react-icons/si';
+import { 
+  SiTwitter,
+  SiGithub,
+  SiNodedotjs,
+  SiGo,
+  SiPython,
+  SiReact,
+  SiGatsby,
+  SiTailwindcss,
+  SiWebpack,
+  SiNextdotjs,
+  SiPrisma,
+  SiMariadb,
+  SiPostgresql,
+  SiMysql,
+  SiGit,
+  SiMongodb,
+  SiRedis,
+  SiVisualstudiocode,
+  SiWebstorm,
+  SiFigma,
+} from 'react-icons/si';
 import { FaLocationArrow } from 'react-icons/fa';
 export default function Home() {
   let [hoveredTimezone, setHoveredTimezone] = useState(false)
@@ -22,7 +43,8 @@ export default function Home() {
         <Nav/>
       </div>
       <MobileNav/>
-      <div className='mx-auto max-w-4xl py-20 px-4'>
+      <div className='mx-auto max-w-4xl py-20 px-4 space-y-12'>
+        <div>
         <h1 className='font-bold text-4xl sm:text-5xl md:text-6xl'>Hi, I&apos;m Misly</h1>
         <div className='flex pt-3'>
           <div className='flex space-x-4 text-2xl bg-neutral-900 w-fit py-2 px-4 rounded-lg items-center divide-x'>
@@ -51,6 +73,73 @@ export default function Home() {
               <FaLocationArrow className='text-lg' />
             </div>
           </div>
+        </div>
+        </div>
+        <div className='space-y-4'>
+          <h1 className='text-2xl sm:text-3xl font-medium'>What do I do?</h1>
+          <p className='text-lg leading-6 tracking-wide text-gray-200 font-light'>I am passionate about technology, from hardware and embedded systems to developing efficient and scalable software. I&apos;m currently working with <Link className='underline' href='https://playcap.app/' target='_blank'>Playcap</Link> where we&apos;re building software targeted at the gaming industry.</p>
+        </div>
+        <div className='space-y-4'>
+          <h1 className='text-2xl sm:text-3xl font-medium'>Technologies</h1>
+          <p className='text-lg leading-6 tracking-wide text-gray-200 font-light'>Over the years I&apos;ve used a variety of languages and tools for many different purposes, I enjoy writing software thats both efficient and easy to scale. Most of the code I have written is in Node.JS, GO and React.</p>
+          <div className='flex space-x-4 text-2xl bg-neutral-900 w-fit py-2 px-4 rounded-lg items-center divide-x'>
+            <ul className='flex items-center w-full justify-evenly flex-wrap'>
+              <li className='p-2'>
+              <SiNodedotjs />
+              </li>
+              <li className='p-2'>
+                <SiGo />
+              </li>
+              <li className='p-2'>
+                <SiPython />
+              </li>
+              <li className='p-2'>
+                <SiReact />
+              </li>
+              <li className='p-2'>
+                <SiGatsby />
+              </li>
+              <li className='p-2'>
+                <SiTailwindcss />
+              </li>
+              <li className='p-2'>
+                <SiWebpack />
+              </li>
+              <li className='p-2'>
+                <SiNextdotjs />
+              </li>
+              <li className='p-2'>
+                <SiPrisma />
+              </li>
+              <li className='p-2'>
+                <SiMariadb />
+              </li>
+              <li className='p-2'>
+                <SiPostgresql />
+              </li>
+              <li className='p-2'>
+                <SiMysql />
+              </li>
+              <li className='p-2'>
+                <SiGit />
+              </li>
+              <li className='p-2'>
+                <SiMongodb />
+              </li>
+              <li className='p-2'>
+                <SiRedis />
+              </li>
+              <li className='p-2'>
+                <SiVisualstudiocode />
+              </li>
+              <li className='p-2'>
+                <SiWebstorm />
+              </li>
+              <li className='p-2'>
+                <SiFigma />
+              </li>
+            </ul>
+            </div>
         </div>
       </div>
     </div>
