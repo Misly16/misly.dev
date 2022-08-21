82b78a2c43d7d778184d315646343bb4a026d053
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-21 12:09:17 +0100

    add cursor click sound

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
+import {useEffect} from 'react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    if (typeof window === "undefined") {
+        return;
+    }
+
+    void new Audio("/cursor.mp3").play().catch(() => null);
+}, [router.pathname]);
   return <Component {...pageProps}/>
 }
 
