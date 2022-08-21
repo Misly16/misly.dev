90c6c5efdbb123e9122685bc9c8d2e543af4b039
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-21 14:59:34 +0100

    remove scroll when mobile nav is expanded

--- components/mobileNav.jsx
+++ components/mobileNav.jsx
@@ -1,38 +1,49 @@
-import { useState } from "react"
-import Link from "next/link"
-import Hamburger from "hamburger-react"
-import { motion } from "framer-motion"
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Hamburger from 'hamburger-react';
+import { motion } from 'framer-motion';
+
 export default function MobileNav() {
-    let [expandedNav, setExpandedNav] = useState(false)
-    const navigation = [
-        {label: 'Home', href: '/'},
-        {label: 'Work', href: '/work'},
-        {label: 'Contact', href: '/contact'},
-      ];
-    return (
-        <>
-    <div>
+  const [expandedNav, setExpandedNav] = useState(false);
+
+  const navigation = [
+    { label: 'Home', href: '/' },
+    { label: 'Work', href: '/work' },
+    { label: 'Contact', href: '/contact' },
+  ];
+
+  useEffect(() => {
+    if (expandedNav) {
+        document.body.style.overflow = 'hidden';
+        return;
+    }
+    document.body.style.overflow = 'unset';
+  }, [expandedNav]);
+
+  return (
+    <>
+      <div>
         { expandedNav && (
-            <div className='sm:hidden z-10 overflow-hidden inset-0 fixed backdrop-blur bg-black/80 py-24 px-10 space-y-7 text-6xl'>
-               {navigation.map((item, index) =>
-                <motion.p   
-                    key={index}
-                    className='scoreboard'
-                    initial={{ opacity: 0}}
-                    animate={{ opacity: 1}}
-                    transition={{ duration:index + .25}}
-                >
-                    <Link key={index} href={item.href}>
-                        {item.label}
-                    </Link>
-                </motion.p>
-            )}
-                </div>
-        )}
+        <div className="sm:hidden z-10 overflow-hidden inset-0 fixed backdrop-blur bg-black/80 py-24 px-10 space-y-7 text-6xl">
+          {navigation.map((item, index) => (
+            <motion.p
+              key={index}
+              className="scoreboard"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: index + 0.25 }}
+            >
+              <Link key={index} href={item.href}>
+                {item.label}
+              </Link>
+            </motion.p>
+          ))}
         </div>
-    <div className={`flex sm:hidden px-3 sticky z-20`}>
-    <Hamburger toggled={expandedNav} toggle={setExpandedNav} />
-    </div>
+        )}
+      </div>
+      <div className="flex sm:hidden px-3 sticky z-20">
+        <Hamburger toggled={expandedNav} toggle={setExpandedNav} />
+      </div>
     </>
- )
+  );
 }
