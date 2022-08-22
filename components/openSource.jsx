ec98e5f56515eb90672fe11ec1c32648e1771df8
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-22 13:41:19 +0100

    open source projects

--- components/openSource.jsx
+++ components/openSource.jsx
@@ -10,7 +10,28 @@ const [repo, setRepo] = useState(null)
   }, [])
     return (
         <div className='flex flex-row flex-wrap w-full space-y-4'>
-            <h1 className='text-2xl sm:text-3xl font-medium'>Open Source</h1>    
-        </div>
+            <h1 className='text-2xl sm:text-3xl font-medium'>Open Source</h1>
+            {repo && repo.map((repo, index) => {
+              return (
+                <div key={index} className="flex text-2xl bg-neutral-900 p-4 rounded-lg w-full max-w-[750px]">
+                <div className='divide-y w-full'>
+                  <div className='flex'>
+                    <div className='flex flex-col justify-center mb-4'>
+                    <p className='text-2xl font-medium'>{repo.repo}</p>
+                    <div className='flex flex-row space-x-3'>
+                      <p className='text-lg font-light'>⭐ {repo.stars}</p>
+                      <p className='text-lg font-light'><span style={{color: `${repo.languageColor}`}}>⬤</span> {repo.language}</p>
+                    </div>
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <p className='text-lg font-medium pt-4'>Description</p>
+                <p className='text-lg font-light text-gray-200'>{repo.description}</p>
+              </div>
+            </div>
+          </div>  
+              )
+            })}
+      </div>
   )
 }
