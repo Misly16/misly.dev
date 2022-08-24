8ed4f08adc8b40d60e02dc6bc8b6fa1e75e19fee
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-24 19:34:39 +0100

    capitalisation

--- components/openSource.jsx
+++ components/openSource.jsx
@@ -12,7 +12,7 @@ const [repo, setRepo] = useState(null)
   }, [])
     return (
         <div className='flex flex-row flex-wrap w-full space-y-4'>
-            <h1 className='text-2xl sm:text-3xl font-medium'>Open Source</h1>
+            <h1 className='text-2xl sm:text-3xl font-medium'>Open source</h1>
             {repo && repo.map((repo, index) => {
               return (
                 <div key={index} className='flex text-2xl bg-neutral-900 p-4 rounded-lg w-full max-w-[750px]'>
