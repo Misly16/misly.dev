1385057c150ed418f234be4e362ce08ea7b02488
Misly <60405462+Misly16@users.noreply.github.com>
2022-08-24 19:44:45 +0100

    add title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import {useEffect} from 'react'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps, router }) {
   useEffect(() => {
@@ -9,7 +10,14 @@ function MyApp({ Component, pageProps, router }) {
 
     void new Audio('/cursor.mp3').play().catch(() => null);
 }, [router.pathname]);
-  return <Component {...pageProps}/>
+  return (
+    <>
+      <Head>
+        <title>Misly</title>
+      </Head>
+      <Component {...pageProps}/> 
+    </>
+    )
 }
 
 export default MyApp
